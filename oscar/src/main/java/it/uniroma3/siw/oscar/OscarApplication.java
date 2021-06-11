51b3c8147f4c10ff9f7ce5e36a3cb66e6b96716e
chiara saccone <72657190+Chiarasaccone22@users.noreply.github.com>
2021-06-11 11:50:13 +0200

    Creazione Progetto

---
+++ oscar/src/main/java/it/uniroma3/siw/oscar/OscarApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.oscar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OscarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OscarApplication.class, args);
+	}
+
+}
